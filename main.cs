2dbd9ea6d672c5cfe6e18d4ddb457deb2f59c4f8
non-user584 <113806065+non-user584@users.noreply.github.com>
2023-09-15 21:46:50 +0000

    Prueba

--- main.cs
+++ main.cs
@@ -1,7 +1,23 @@
+class Test
+{
+    public int Prop1 {get; set;}
+
+    public Test()
+    {
+        this.Prop1 = 10;   
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine(this.Prop1);
+    }
+}
 class program
 {
     static void Main()
     {
+        var p = new Test();
+        p.Mostrar();
         Console.WriteLine("Exito!!!");
     }
 }
